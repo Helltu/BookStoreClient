0fcc3b3c9ba943b7ebfcfaa39b3a8d1760d77442
Hellty <korsovm@gmail.com>
2026-04-19 12:34:34 +0300

    Feat: Initial set up

---
+++ lib/api/axios.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'http://localhost:8080/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    // Получаем токен из localStorage, если мы на клиенте
+    const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+    
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
